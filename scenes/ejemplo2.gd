50f71ef9c3e716a902466debaaa443f6bd3af587
cstammp <cstammp@gmail.com>
2025-04-21 16:33:34 -0400

    refactor: delete unused files

--- scenes/ejemplo2.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-#set('charge',1)
-#charge=-1
-#vector= Vector2(0,1000)
-#
--- script/fields/ejemplo1.gd
+++
@@ -1,7 +0,0 @@
-extends Zona
-
-func on_ready():
-	charge=-1
-	print(vector)
-	setCharge(Vector2(0,10000),-1)
-	print(vector)
