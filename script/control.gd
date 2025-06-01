a3a6241b9e68654cf011e5247d82f5789bde7407
cstammp <cstammp@gmail.com>
2025-06-01 14:34:20 -0400

    minor fix in level selector

--- script/control.gd
+++ script/control.gd
@@ -2,25 +2,17 @@ extends Control
 
 const LEVEL_BTN = preload("res://scenes/interface/button.tscn")
 
-@export_dir var dir_path
 @onready var grid: GridContainer = $"../GridContainer"
 
-func _ready() -> void:
-	get_levels(dir_path)
+@export var level_paths: Array[String] = [
+	"res://scenes/levels/demo_level.tscn",
+	"res://scenes/levels/demo_level_2.tscn"
+]
 
-func get_levels(path):
-	var dir = DirAccess.open(path)
-	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			if not dir.current_is_dir() and file_name.ends_with(".tscn"):
-				var full_path = dir.get_current_dir().path_join(file_name)
-				create_level_btn(full_path, file_name)
-			file_name = dir.get_next()
-		dir.list_dir_end()
-	else:
-		print("An error occurred when trying to access the path.")
+func _ready() -> void:
+	for lvl_path in level_paths:
+		var name = lvl_path.get_file().trim_suffix(".tscn").replace("_", " ")
+		create_level_btn(lvl_path, name)
 
 func create_level_btn(lvl_path: String, lvl_name: String):
 	var btn = LEVEL_BTN.instantiate()
