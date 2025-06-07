5c1a4b5dfb88f4641bf63d9bfa4363d67a41158c
cristobal207 <cristobal.puentes@ug.uchile.cl>
2025-06-06 21:54:03 -0400

    feat: the plataform can change their velocity

--- script/platforms/lineal_platform.gd
+++ script/platforms/lineal_platform.gd
@@ -2,13 +2,14 @@ extends AnimatableBody2D
 
 var pos1: Vector2
 var pos2: Vector2
+@export var distancia:Vector2 
 var from: Vector2
 var to: Vector2
 var time: float = 0
-
+@export var speed: float = 100.0
 func _ready() -> void:
 	pos1 = self.position
-	pos2 = pos1 + Vector2(200, 0)
+	pos2 = pos1 + distancia
 
 func _physics_process(delta: float) -> void:
 	
@@ -17,7 +18,11 @@ func _physics_process(delta: float) -> void:
 	if self.position == pos2:
 		_changeDirection(pos2, pos1)
 	
-	time = min(time+delta, 1)
+	var total_dist := distancia.length()
+	if total_dist > 0.0:
+		time = min(time + delta * speed / total_dist, 1.0)
+	else:
+		time = 1.0 
 	
 	self.position = time * to + (1 - time) * from
 	#print(self.position)
